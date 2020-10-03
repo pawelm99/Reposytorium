24f1a3f5159c3b3cad53e2908e20acae87bd1426
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2020-10-03 13:34:59 +0200

    gg

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,11 +2,20 @@
 
 namespace ConsoleApp2
 {
+   
     class Program
     {
+        enum Predkosc
+        {
+            Szybko,
+            Wolno,
+            BardzoSzybko,
+        }
+       
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+   
         }
     }
 }
