1677eafeeae460c60d7d0cc53b97d170e303c37f
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2020-10-20 14:54:04 +0200

    Zadanie 2 pop

--- Zadanie2/Program.cs
+++ Zadanie2/Program.cs
@@ -1,60 +1,63 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text.RegularExpressions;
 
 
 namespace ConsoleApp2
 {
-   
     class Program
     {
-
         enum MiejscaZ
         {
-            
-            Brak,
-            Jedno,
-            Dwa,
-            
+            Brak = 0,
+            Jedno = 1,
+            Dwa = 2,
         }
-        
+
+        static MiejscaZ iloscMiejsc;
+
         static void Main(string[] args)
         {
-            
-            
             for (;;)
             {
-            
-            var liczbaA = Console.ReadLine();
-            var liczbaB = Console.ReadLine();
-            var liczbaC = Console.ReadLine();
+                var liczbaA = Console.ReadLine();
+                var liczbaB = Console.ReadLine();
+                var liczbaC = Console.ReadLine();
 
-           
-                
-            var czyUdaloSie1 = int.TryParse(liczbaA, out int a);
-            var czyUdaloSie2 = int.TryParse(liczbaB, out int b);
-            var czyUdaloSie3 = int.TryParse(liczbaC, out int c);
 
-            
+                var czyUdaloSie1 = int.TryParse(liczbaA, out int a);
+                var czyUdaloSie2 = int.TryParse(liczbaB, out int b);
+                var czyUdaloSie3 = int.TryParse(liczbaC, out int c);
+
+
                 if ((czyUdaloSie1) && (czyUdaloSie2) && (czyUdaloSie3))
                 {
-                    var delta =(Math.Pow(b, 2)) - (4 * a * c);
-                    Console.WriteLine("Delta: {0}",delta);
-                    var wynik=0;
-                    
-                    if (delta > 1) wynik = 2;
-                    else if (delta == 0) wynik = 1;
-                    else wynik = 0;
+                    var delta = (Math.Pow(b, 2)) - (4 * a * c);
+                    Console.WriteLine("Delta: {0}", delta);
+                    var pDelta = Math.Sqrt(delta);
+                    string wynik;
 
-                    var z = (wynik) switch
+                    if (delta > 1)
                     {
-                        (0) => MiejscaZ.Brak,
-                        (1) => MiejscaZ.Jedno,
-                        (2) => MiejscaZ.Dwa,
-                        
-                    };
-                    
-                    Console.WriteLine("Liczba miejsc zerowych: {0}", z);
+                        iloscMiejsc = MiejscaZ.Dwa;
+                        var placeZeroOne = (-b - pDelta) / (2 * a);
+                        var placeZeroTwo = (-b + pDelta) / (2 * a);
+                        wynik = $"Miejsca: {placeZeroOne:F} {placeZeroTwo:F}";
+                    }
+                    else if (delta == 0)
+                    {
+                        iloscMiejsc = MiejscaZ.Jedno;
+                        var placeZero = -b / (2 * a);
+                        wynik = $"Miejsca: {placeZero:F}";
+                    }
+                    else
+                    {
+                        iloscMiejsc = MiejscaZ.Brak;
+                        wynik = $"Miejsca: Brak";
+                    }
+
+                    Console.WriteLine("Liczba miejsc zerowych: {0}\n{1}", iloscMiejsc,wynik);
                     break;
                 }
                 else
