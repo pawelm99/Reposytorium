eb8afd4c5f8a68fee61358f756ef9873ca556514
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2020-10-31 10:12:10 +0100

    Zadanie4

---
+++ Zadanie4/ConsoleApp4/Kasiazka.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    public class Kasiazka
+    {
+        public string Tytul { get; set; }
+        public string Autor { get; set; }
+        private int _numerRegalu;
+        private int _numerPolki;
+        private int _nrMiejNaPolce;
+
+        public Kasiazka(int numerRegalu, int numerPolki, int nrMiejNaPolce, string tytul, string autor)
+        {
+            _numerRegalu = numerRegalu;
+            _numerPolki = numerPolki;
+            _nrMiejNaPolce = nrMiejNaPolce;
+            Tytul = tytul;
+            Autor = autor;
+        }
+
+        public override string ToString()
+        {
+            return $"Regał: {_numerRegalu}, Półka: {_numerPolki}, Miejsce na półce:: {_nrMiejNaPolce}, {nameof(Tytul)}: {Tytul}, {nameof(Autor)}: {Autor}";
+        }
+    }
+}
---
+++ Zadanie4/ConsoleApp4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kasiazka[,,] tab = new Kasiazka[3, 6, 10];
+
+            wypelnij(tab);
+            tab[2, 2, 1].Autor = "Aleksander Fredro";
+            tab[2, 2, 1].Tytul = "Zemsta";
+
+            var wczytaj = Console.ReadLine();
+            show(tab,wczytaj);
+        }
+
+        private static void show(Kasiazka[,,] tab,string wczytaj)
+        {
+            for (int i = 0; i < tab.GetLength(0); i++)
+            {
+                for (int j = 0; j < tab.GetLength(1); j++)
+                {
+                    for (int k = 0; k < tab.GetLength(2); k++)
+                    {
+                        if ((tab[i,j,k].Tytul.StartsWith(wczytaj)) || (tab[i,j,k].Autor.StartsWith(wczytaj)))
+                        {
+                            Console.WriteLine(tab[i, j, k]);
+                        }
+                        
+                        
+                    }
+                }
+            }
+        }
+
+        private static void wypelnij(Kasiazka[,,] tab)
+        {
+            for (int i = 0; i < tab.GetLength(0); i++)
+            {
+                for (int j = 0; j < tab.GetLength(1); j++)
+                {
+                    for (int k = 0; k < tab.GetLength(2); k++)
+                    {
+                        tab[i,j,k] = new Kasiazka(i,j,k,"Pan Tadeusz","Adam Mickiewicz");
+                    }
+                }
+            }
+        }
+    }
+}
