8f55e5a14db8dbbc1a09588b95a904fcb9e0e343
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2020-10-31 14:58:48 +0100

    Poprawione

--- Zadanie4/ConsoleApp4/Program.cs
+++ Zadanie4/ConsoleApp4/Program.cs
@@ -10,8 +10,11 @@ namespace ConsoleApp4
             Kasiazka[,,] tab = new Kasiazka[3, 6, 10];
 
             wypelnij(tab);
-            tab[2, 2, 1].Autor = "Aleksander Fredro";
-            tab[2, 2, 1].Tytul = "Zemsta";
+            tab[1, 1, 5].Autor = "J.K. Rowling";
+            tab[1, 1, 5].Tytul = "Harry Potter i Czara Ognia";
+          
+            
+            
 
             var wczytaj = Console.ReadLine();
             show(tab,wczytaj);
@@ -25,7 +28,8 @@ namespace ConsoleApp4
                 {
                     for (int k = 0; k < tab.GetLength(2); k++)
                     {
-                        if ((tab[i,j,k].Tytul.StartsWith(wczytaj)) || (tab[i,j,k].Autor.StartsWith(wczytaj)))
+                        //if ((tab[i,j,k].Tytul.StartsWith(wczytaj)) || (tab[i,j,k].Autor.StartsWith(wczytaj)))
+                        if ((System.Text.RegularExpressions.Regex.IsMatch(tab[i,j,k].Tytul, wczytaj)) || (System.Text.RegularExpressions.Regex.IsMatch(tab[i,j,k].Autor, wczytaj)))
                         {
                             Console.WriteLine(tab[i, j, k]);
                         }
