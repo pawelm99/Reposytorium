2d74b47d0a3b7393aecf57a1db71ac476fdce8fb
pawelm99 <71591670+pawelm99@users.noreply.github.com>
2021-01-20 17:36:05 +0100

    OK

---
+++ PM055928/ConsoleApp15/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
